ccc126e76cab0f267c788f8ff9f8fa624c051190
songhanlu <1722382969@qq.com>
2018-01-05 18:24:33 +0800

    20180105

--- src/main/java/com/bdqn/controller/ScoreController.java
+++ src/main/java/com/bdqn/controller/ScoreController.java
@@ -1,7 +1,16 @@
 package com.bdqn.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.bdqn.pojo.Score;
+import com.bdqn.service.score.ScoreService;
+import com.bdqn.tools.PageUtil;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * Created by hp on 2017/12/26.
@@ -9,8 +18,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/score")
 public class ScoreController {
+    @Resource
+    private ScoreService scoreService;
     @RequestMapping("/toScore")
     public String toScore(){
         return "score/score";
+    }
+
+    @RequestMapping(value = "/findAllScore",method = RequestMethod.GET,
+                    produces = {"application/json;charset=utf-8"})
+    @ResponseBody
+    public String findAllScore(Integer page,
+                               Integer rows,
+                               Integer studentID,
+                               Integer gradeID){
+        if(studentID!=null && studentID==-1){
+            studentID=null;
+        }
+        if(gradeID!=null && gradeID==-1){
+            gradeID=null;
+        }
+        PageInfo<Score> pageInfo = scoreService.findAllScore(page, rows, studentID, gradeID);
+        return JSON.toJSONString(new PageUtil<Score>(pageInfo));
     }
 }
