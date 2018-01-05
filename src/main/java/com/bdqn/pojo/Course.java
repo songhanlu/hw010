386431ba2846319b2e4f212a593ad04e5846a4c2
songhanlu <1722382969@qq.com>
2018-01-05 15:28:54 +0800

    20180105

--- src/main/java/com/bdqn/pojo/Course.java
+++ src/main/java/com/bdqn/pojo/Course.java
@@ -7,4 +7,27 @@ public class Course {
     private Integer id;
     private String course_name;
 
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", course_name='" + course_name + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
 }
